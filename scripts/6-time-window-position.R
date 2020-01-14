19c5da47c42b1a2238aa57029ad54b4ad6ac6bdd
Alec Robitaille <robit.alec@gmail.com>
2020-01-14 14:48:06 -0330

    fst time window position

---
+++ scripts/6-time-window-position.R
@@ -0,0 +1,127 @@
+### Scale in multilayer networks - time windows
+# Alec Robitaille
+
+### Packages ----
+pkgs <- c('data.table',
+					'ggplot2',
+					'spatsoc',
+					'rgdal',
+					'asnipe',
+					'igraph')
+p <- lapply(pkgs, library, character.only = TRUE)
+
+
+### Data ----
+DT <- fread('data/FogoCaribou.csv')
+
+### Sub data ----
+# TODO: add season? 
+sub <- DT[Year == 2018]
+
+idcol <- 'ANIMAL_ID'
+# keepids <- sub[, .N, c(idcol, 'season')][, .N, idcol][N == 2][[idcol]]
+# sub <- sub[get(idcol) %chin% keepids]
+
+### Cast columns ----
+sub[, idate := as.IDate(idate)]
+sub[, itime := as.ITime(itime)]
+
+### Project relocations ----
+# UTM zone 21N
+projCols <- c('EASTING', 'NORTHING')
+utm21N <- '+proj=utm +zone=21 ellps=WGS84'
+
+sub[, (projCols) := as.data.table(project(cbind(X_COORD, Y_COORD), utm21N))]
+
+
+### Variable time window length ----
+winlengths <- seq(75, 150, 5)
+  
+lapply(winlengths, function(l) {
+  col <- paste0('season', l)
+  
+  sub[between(JDate, 1, 1 + l), (col) := 'winter']
+  sub[between(JDate, 215, 215 + l), (col) := 'summer']
+})
+
+
+### Temporal grouping ----
+tempthresh <- '5 minutes'
+
+group_times(
+  sub,
+  datetime =  c('idate', 'itime'),
+  threshold = tempthresh
+)
+
+### Generate networks for each n observations ----
+spatthresh <- 50
+
+# TODO: Careful not including "season" and "season75" after prep
+seasoncols <- colnames(sub)[grepl('^season', colnames(sub))]
+
+graphs <- lapply(seasoncols, function(col) {
+  # Spatial grouping with spatsoc
+  group_pts(
+    sub,
+    threshold = spatthresh,
+    id = idcol,
+    coords = projCols,
+    timegroup = 'timegroup',
+    splitBy = col
+  )
+  
+  usplit <- unique(na.omit(sub, cols = col)[[col]])
+  
+  # GBI for each season
+  gbiLs <- lapply(usplit, function(u) {
+    gbi <- get_gbi(
+      DT = sub[get(col) == u],
+      group = 'group',
+      id = idcol
+    )
+  })
+
+  # Generate networks for each season
+  netLs <- lapply(
+    gbiLs,
+    get_network,
+    data_format = 'GBI',
+    association_index = 'SRI'
+  )
+
+  gLs <- lapply(
+    netLs,
+    graph.adjacency,
+    mode = 'undirected',
+    diag = FALSE,
+    weighted = TRUE
+  )
+  names(gLs) <- paste0(col, '-', usplit)
+  gLs
+})
+
+### Multilayer network metrics ----
+# TODO: difference in degree? between seasons
+
+# TODO: why multiple for same individual
+multdeg <- rbindlist(lapply(graphs, function(g) {
+  deg <- lapply(g, degree)
+  rbindlist(lapply(deg, stack), idcol = 'by')
+}))
+
+setnames(multdeg, c('by', 'deg', idcol))
+
+multdeg[, c('winlength', 'season') := tstrsplit(by, '-', type.convert = TRUE)]
+multdeg[, winlength := as.integer(gsub('season', '', winlength))]
+multdeg[, mdeg := sum(deg), by = c('by', idcol)]
+
+
+### Plots ----
+ggplot(multdeg) + 
+  geom_line(aes(winlength, deg)) +
+  facet_grid(season~ANIMAL_ID)
+
+
+
+
