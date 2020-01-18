45047e559fc0c44c49d6c81ad9591a57146a3a3c
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-18 15:14:37 -0400

    fst net correlations across num obs

--- scripts/5-number-of-observations.R
+++ scripts/5-number-of-observations.R
@@ -113,7 +113,49 @@ multdeg[, c('nobs', 'season') := tstrsplit(by, '-', type.convert = TRUE)]
 multdeg[, mdeg := sum(deg), by = c('by', idcol)]
 
 
+# Network correlations
+netcors <- lapply(seq(2, maxn, by = 2), function(n) {
+  nsub <- sub[timegroup %in% randobs[1:n]]
+  group_pts(
+    nsub,
+    threshold = spatthresh,
+    id = idcol,
+    coords = projCols,
+    timegroup = 'timegroup',
+    splitBy = splitBy
+  )
+  
+  usplit <- unique(nsub[[splitBy]])
+  gbiLs <- lapply(usplit, function(u) {
+    gbi <- get_gbi(
+      DT = nsub[get(splitBy) == u],
+      group = 'group',
+      id = idcol
+    )
+  })
+  
+  netLs <- lapply(
+    gbiLs,
+    get_network,
+    data_format = 'GBI',
+    association_index = 'SRI'
+  )
+  cor(c(netLs[[1]]), c(netLs[[2]]))
+})
+netcors <- data.table(
+  cornet = vapply(seq_along(netLs)[-length(netLs)], function(i) {
+    cor(c(netLs[[i]]), c(netLs[[i + 1]]))
+  }, FUN.VALUE = 42.0),
+  spatscale = unique(ml$nobs)[-length(netLs)]
+)
+
+
+
 ### Plots ----
+ggplot(netcors) +
+  geom_line(aes(spatscale, cornet))
+
+
 ggplot(multdeg) + 
   geom_line(aes(nobs, mdeg, color = get(idcol), group = get(idcol))) +
   facet_wrap(~get(idcol)) +
