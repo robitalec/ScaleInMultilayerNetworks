068fddb253c36fc3b36974d595e167933a99e9ea
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-19 16:44:39 -0400

    setup function

--- R/neighbours.R
+++ R/neighbours.R
@@ -1,12 +1,48 @@
 nsub
 
 
+neigh <- function(DT, id, splitBy) {
+  if (any(!(c(id, 'group', splitBy) %in% colnames(DT)))) {
+    stop(paste0(
+      as.character(paste(setdiff(
+        c(id, 'group', splitBy), colnames(DT)
+      ), collapse = ", ")),
+      " field(s) provided are not present in input DT"
+    ))
+  }
+  
+  # with splitBy
+  }
+  DT[, {
+    g <- group
+    DT[group %in% g, data.table::uniqueN(get(id))]
+  },
+  by = c(splitBy, id)][
+    DT[, {
+      g <- group
+      DT[group %in% g, data.table::uniqueN(get(id))]
+      },
+      by = id],
+  on = id]
+  
+  # 
+  # DT[, {
+  #   g <- group
+  #   DT[group %in% g, data.table::uniqueN(get(id))]
+  # },
+  # c(splitBy, id)]
+  
+  
+  
+}
 
-nsub[, {
-  g <- group
-  nsub[group %in% g, uniqueN(ANIMAL_ID)]}, 
-     .(season, ANIMAL_ID)]
+neigh(nsub, 'ANIMAL_ID', 'season')
+# nsub[, {
+#   g <- group
+#   nsub[group %in% g, uniqueN(ANIMAL_ID)]}, 
+#      .(season, ANIMAL_ID)]
 
 
-nsub[group %in% nsub[ANIMAL_ID == 'FO2017003', group],
-     uniqueN(ANIMAL_ID)]
+# nsub[group %in% nsub[ANIMAL_ID == 'FO2017003', group],
+#      uniqueN(ANIMAL_ID)]
+
