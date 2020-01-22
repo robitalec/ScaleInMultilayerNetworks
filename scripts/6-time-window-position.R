dc56be48dd909dc8355a39203469494059ac9c17
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-22 13:28:05 -0400

    fin tidy 6-window pos

--- scripts/6-time-window-position.R
+++ scripts/6-time-window-position.R
@@ -10,54 +10,43 @@ pkgs <- c('data.table',
 p <- lapply(pkgs, library, character.only = TRUE)
 
 
+### Variables ----
+source('scripts/0-variables.R')
+
+
 ### Data ----
-DT <- fread('data/FogoCaribou.csv')
-
-### Sub data ----
-sub <- DT[Year == 2018]
-
-idcol <- 'ANIMAL_ID'
-
-### Cast columns ----
-sub[, idate := as.IDate(idate)]
-sub[, itime := as.ITime(itime)]
-
-### Project relocations ----
-# UTM zone 21N
-projCols <- c('EASTING', 'NORTHING')
-utm21N <- '+proj=utm +zone=21 ellps=WGS84'
-
-sub[, (projCols) := as.data.table(project(cbind(X_COORD, Y_COORD), utm21N))]
+DT <- readRDS('data/derived-data/sub-2018-fogo-caribou.Rds')
+alloc.col(DT)
 
 
 ### Variable time window position ----
 winlength <- 75
 winpositions <- seq(1, 75, by = 1)
   
-lapply(winpositions, function(pos) {
+l <- lapply(winpositions, function(pos) {
   col <- paste0('season', pos)
   
-  sub[between(JDate, 1 + pos, 1 + pos + winlength), (col) := 'winter']
-  sub[between(JDate, 215 + pos, 215 + pos + winlength), (col) := 'summer']
+  DT[between(JDate, 1 + pos, 1 + pos + winlength), (col) := 'winter']
+  DT[between(JDate, 215 + pos, 215 + pos + winlength), (col) := 'summer']
+  
+  # Fake output
+  l
 })
 
 
 ### Temporal grouping ----
-tempthresh <- '5 minutes'
-
 group_times(
-  sub,
-  datetime =  c('idate', 'itime'),
+  DT,
+  datetime =  c(datecol, timecol),
   threshold = tempthresh
 )
 
 ### Generate networks for each n observations ----
-spatthresh <- 50
-
-# TODO: Careful not including "season" and "season75" after prep
-seasoncols <- colnames(sub)[grepl('^season', colnames(sub))]
-
-graphs <- lapply(seasoncols, function(col) {
+nets <- lapply(winpositions, function(pos) {
+  col <- paste0('season', pos)
+  
+  sub <- DT[!is.na(get(col))]
+  
   # Spatial grouping with spatsoc
   group_pts(
     sub,
@@ -68,56 +57,68 @@ graphs <- lapply(seasoncols, function(col) {
     splitBy = col
   )
   
-  usplit <- unique(na.omit(sub, cols = col)[[col]])
+  # usplit <- unique(na.omit(sub, cols = col)[[col]])
+  # 
+  # # GBI for each season
+  # gbiLs <- lapply(usplit, function(u) {
+  #   gbi <- get_gbi(
+  #     DT = sub[get(col) == u],
+  #     group = 'group',
+  #     id = idcol
+  #   )
+  # })
+  # 
+  # # Generate networks for each season
+  # netLs <- lapply(
+  #   gbiLs,
+  #   get_network,
+  #   data_format = 'GBI',
+  #   association_index = 'SRI'
+  # )
+  # 
+  # gLs <- lapply(
+  #   netLs,
+  #   graph.adjacency,
+  #   mode = 'undirected',
+  #   diag = FALSE,
+  #   weighted = TRUE
+  # )
+  # names(gLs) <- paste0(col, '-', usplit)
+  # gLs
   
-  # GBI for each season
-  gbiLs <- lapply(usplit, function(u) {
-    gbi <- get_gbi(
-      DT = sub[get(col) == u],
-      group = 'group',
-      id = idcol
-    )
-  })
-
-  # Generate networks for each season
-  netLs <- lapply(
-    gbiLs,
-    get_network,
-    data_format = 'GBI',
-    association_index = 'SRI'
-  )
-
-  gLs <- lapply(
-    netLs,
-    graph.adjacency,
-    mode = 'undirected',
-    diag = FALSE,
-    weighted = TRUE
-  )
-  names(gLs) <- paste0(col, '-', usplit)
-  gLs
+  neigh(sub, idcol, col)
+  
+  outcols <- c('neighborhood', 'splitNeighborhood', idcol, col)
+  out <- unique(sub[, .SD, .SDcols = outcols])
+  setnames(out, col, 'season')
+  set(out, j = 'winpos', value = pos)
+  
 })
 # TODO: check why different number of individuals as seasons move
-
-
-
-### Multilayer network metrics ----
 # TODO: difference in degree? between seasons
 # TODO: why multiple for same individual
-multdeg <- rbindlist(lapply(graphs, function(g) {
-  deg <- lapply(g, degree)
-  rbindlist(lapply(deg, stack), idcol = 'by')
-}))
-
-setnames(multdeg, c('by', 'deg', idcol))
-
-multdeg[, c('winpos', 'season') := tstrsplit(by, '-', type.convert = TRUE)]
-multdeg[, winpos := as.integer(gsub('season', '', winpos))]
-multdeg[, mdeg := sum(deg), by = c('by', idcol)]
-
-
-### Plots ----
 # TODO: careful if inconsistent number of individuals
+
+out <- rbindlist(nets)
+
+### Multilayer network metrics ----
+var <- 'winpos'
+
+# Redundancy
+redundancy(out)
+stopifnot(out[!between(connredund, 0, 1), .N] == 0)
+
+# Multidegree
+multidegree(out, 'splitNeighborhood', idcol, var)
+
+# Degree deviation
+degdeviation(out, 'splitNeighborhood', idcol, var)
+
+# Relevance
+relevance(out, idcol, var, splitBy = splitBy)
+stopifnot(out[!between(relev, 0, 1), .N] == 0)
+
+# TODO: network correlation
 
 
 ### Output ----
