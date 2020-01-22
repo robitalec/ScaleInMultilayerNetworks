7358cab7a13d97918168cd5473745bf197d4d42c
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-21 20:53:37 -0400

    variables sections

--- scripts/0-variables.R
+++ scripts/0-variables.R
@@ -1,11 +1,20 @@
+## ID
 idcol <- 'ANIMAL_ID'
+
+## Spatial
+utm21N <- '+proj=utm +zone=21 ellps=WGS84'
+projCols <- c('EASTING', 'NORTHING')
+xcol <- 'X_COORD'
+ycol <- 'Y_COORD'
+
+## Temporal
 datecol <- 'idate'
 timecol <- 'itime'
-
-splitBy <- 'season'
 
 # Seasons
 winterlow <- 1
 winterhigh <- 75
 summerlow <- 215
 summerhigh <- 290
+
+splitBy <- 'season'
