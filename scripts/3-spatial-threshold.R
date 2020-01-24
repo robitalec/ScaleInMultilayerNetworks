087dc4f2067b41e7e612ac21b1ac10a8128a910d
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-24 13:58:34 -0400

    update spatial threshold

--- scripts/3-spatial-threshold.R
+++ scripts/3-spatial-threshold.R
@@ -19,121 +19,71 @@ source('scripts/0-variables.R')
 DT <- readRDS('data/derived-data/sub-seasons-fogo-caribou.Rds')
 alloc.col(DT)
 
-### Spatiotemporal grouping with spatsoc ----
-# list spatial thresholds
-thresholds <- c(5, seq(50, 500, by = 50))
-
+### Temporal grouping with spatsoc ----
 group_times(
   DT,
   datetime =  c(datecol, timecol),
   threshold = '10 minutes'
 )
 
-nets <- lapply(thresholds, function(t) {
+### Generate networks for each spatial threshold ----
+# list spatial thresholds
+thresholds <- c(5, seq(50, 500, by = 50))
 
-	neigh(DT, idcol, splitBy)
-	
-	out <- unique(DT[, .SD, 
-	                 .SDcols = c('neighborhood', 'splitNeighborhood', idcol, splitBy)])
-	set(out, j = 'spatialthreshold', value = t)
-})
+var <- 'spatialthreshold'
+
 
 nets <- lapply(thresholds, function(t) {
+  col <- splitBy
+  
+  # No sub for spatial threshold
+  sub <- DT
+  
+  # Spatial grouping with spatsoc
   group_pts(
-    DT,
-    threshold = t,
+    sub,
+    threshold = spatthresh,
     id = idcol,
     coords = projCols,
-    timegroup = 'timegroup'
+    timegroup = 'timegroup',
+    splitBy = col
   )
   
   usplit <- unique(na.omit(sub, cols = col)[[col]])
   
   # GBI for each season
-  gbiLs <- lapply(usplit, function(u) {
-    gbi <- get_gbi(
-      DT = sub[get(col) == u],
-      group = 'group',
-      id = idcol
-    )
-  })
+  gbiLs <- list_gbi(sub, idcol, usplit, col)
   
   # Generate networks for each season
-  netLs <- lapply(
-    gbiLs,
-    get_network,
-    data_format = 'GBI',
-    association_index = 'SRI'
-  )
+  netLs <- list_nets(gbiLs)
   
-  gLs <- lapply(
-    netLs,
-    graph.adjacency,
-    mode = 'undirected',
-    diag = FALSE,
-    weighted = TRUE
-  )
-  names(gLs) <- paste0(col, '-', usplit)
+  # Generate graphs for each season
+  gLs <- list_graphs(netLs)
+  names(gLs) <- paste0(t, '-', usplit)
   
-  eig <- rbindlist(
-    lapply(lapply(gLs, function(g) eigen_centrality(g)$vector), stack),
-    idcol = 'lenseason')
-  eig[, c('winlength', 'season') := tstrsplit(lenseason, '-')]
-  eig[, winlength := as.integer(gsub('season', '', winlength))]
-  setnames(eig, c('ind', 'values'), c(idcol, 'eigcent'))
+  # Calculate eigenvector centrality for each season
+  eig <- layer_eigen(gLs)
   
-  # TODO: modify neigh so it's working on a by
-  # TODO: remove it from this lapply and merge afterwards
-  # TODO: just return eig centrality and network correlation etc
-  # TODO: then merge onto neigh output from above
-  neigh(sub, idcol, col)
+  # Calculate correlation of season layers
+  set(eig, j = 'layercorr', value = layer_correlation(gLs))
+  eig[, c(var, splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
+  setnames(eig, 'ind', idcol)
   
-  outcols <- c('neighborhood', 'splitNeighborhood', idcol, col)
+  # Calculate neighbors
+  layer_neighbors(sub, idcol, splitBy = col)
+  
+  # and tidy output, prep for merge
+  outcols <- c('neigh', 'splitNeigh', idcol, col)
   out <- unique(sub[, .SD, .SDcols = outcols])
-  setnames(out, col, 'season')
-  set(out, j = 'winlength', value = len)
+  setnames(out, col, splitBy)
   
-  out <- unique(DT[, .SD, 
-                   .SDcols = c('neighborhood', 'splitNeighborhood', idcol, splitBy)])
-  set(out, j = 'spatialthreshold', value = t)
+  # Merge eigcent+correlations with neighbors
+  out[eig, on = c(idcol, splitBy)]
   
-  out[eig, on = c(idcol, 'season', 'winlength')]
 })
 
 out <- rbindlist(nets)
 
 
-### Multilayer network metrics ----
-var <- 'spatialthreshold'
-
-# Redundancy
-redundancy(out)
-stopifnot(out[!between(connredund, 0, 1), .N] == 0)
-
-# Multidegree
-multidegree(out, 'splitNeighborhood', idcol, var)
-
-# Degree deviation
-degdeviation(out, 'splitNeighborhood', idcol, var)
-
-# Relevance
-# TODO: fix implementation of var
-relevance(out, idcol, splitBy = c(var, splitBy))
-stopifnot(out[!between(relev, 0, 1), .N] == 0)
-
-# TODO: network correlation
-
 ### Output ----
 saveRDS(out, 'data/derived-data/3-spatial-threshold.Rds')
-
-
-
-## ARCHIVE
-dcast(ml, ANIMAL_ID ~ get(var), value.var = 'cent')
-
-# Network correlations
-netcors <- data.table(
-  cornet = vapply(seq_along(netLs)[-length(netLs)], function(i) {
-  cor(c(netLs[[i]]), c(netLs[[i + 1]]))
-}, FUN.VALUE = 42.0),
-spatscale = unique(ml$spatscale)[-length(netLs)])
