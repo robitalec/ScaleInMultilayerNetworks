79a2860eebf2c873f9ad5756785ba7a880a4359d
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-24 13:43:19 -0400

    tidy ml net metrics section

--- scripts/4-time-window-length.R
+++ scripts/4-time-window-length.R
@@ -99,49 +99,23 @@ graphLs <- lapply(winlengths[1:2], function(len) {
 
 
 ### Multilayer network metrics ----
-
-gLs <- unlist(graphLs, recursive = FALSE)
-eig <- layer_eigen(gLs, idcol)
-
-eig[, c(var, splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
-
-
-layer_neighbors(sub, idcol, var, col)
-
-
-# TODO: modify neigh so it's working on a by
-# TODO: then merge onto neigh output from above
-# 
-# outcols <- c('neighborhood', 'splitNeighborhood', idcol, col)
-# out <- unique(sub[, .SD, .SDcols = outcols])
-# setnames(out, col, 'season')
-# set(out, j = 'winlength', value = len)
-# 
-# out[eig, on = c(idcol, 'season', 'winlength')]
-
 # TODO: fix new data 'found duplicate id in a timegroup and/or splitBy - does your group_times threshold match the fix rate?'
-nnets <- unlist(nets, recursive = FALSE)
-layer_eigen(nnets)
-layer_
+
 out <- rbindlist(nets)
 
-var <- 'winlength'
-
 # Redundancy
-redundancy(out)
+connective_redudancy(out)
 stopifnot(out[!between(connredund, 0, 1), .N] == 0)
 
 # Multidegree
-multidegree(out, 'splitNeighborhood', idcol, var)
+multi_degree(out, 'splitNeigh', idcol, var)
 
 # Degree deviation
-degdeviation(out, 'splitNeighborhood', idcol, var)
+deviation_degree(out, 'splitNeigh', idcol, var)
 
 # Relevance
-relevance(out, idcol, var, splitBy = splitBy)
+layer_relevance(out, idcol, var, splitBy = splitBy)
 stopifnot(out[!between(relev, 0, 1), .N] == 0)
-
-# TODO: network correlation
 
 ### Output ----
 saveRDS(out, 'data/derived-data/4-time-window-length.Rds')
