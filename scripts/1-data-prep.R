fc828a4530f52a53d8a1c65c60c456c4f8eb7bf0
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-26 13:14:23 -0400

    fix bug include.lowest=TRUE

--- scripts/1-data-prep.R
+++ scripts/1-data-prep.R
@@ -41,11 +41,11 @@ rcl <- matrix(c(
 ncol = 2)
 rclnms <- list(open = 1, forest = 2, lichen = 3)
 
-reclass <- reclassify(mlc, rcl)
+reclass2 <- reclassify(lc, rcl, include.lowest = TRUE)
 
 
 ### Landcover sample ----
-DT[, 'lc30' := extract(reclass, matrix(c(EASTING, NORTHING), ncol = 2))]
+DT[, 'lc302' := extract(reclass2, matrix(c(EASTING, NORTHING), ncol = 2))]
 
 ### Sub data ----
 ## 2017 summer and 2018 winter
