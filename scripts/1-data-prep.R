cf2a1a7805f0c06c0a68675aed6c31059d94597b
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-25 20:01:05 -0400

    drop ids
    only those individuals with data in both seasons, when seasons move

--- scripts/1-data-prep.R
+++ scripts/1-data-prep.R
@@ -20,6 +20,8 @@ DT[, c(datecol, timecol) := .(as.IDate(get(datecol)), as.ITime(get(timecol)))]
 ## Seasons
 DT[between(JDate, winterlow, winterhigh), season := 'winter']
 DT[between(JDate, summerlow, summerhigh), season := 'summer']
+
+
 ### Project relocations ----
 DT[, (projCols) := as.data.table(project(cbind(get(xcol), get(ycol)), utm21N))]
 
@@ -28,10 +30,11 @@ DT[, (projCols) := as.data.table(project(cbind(get(xcol), get(ycol)), utm21N))]
 sub <- DT[Year >= 2017 & JDate > summerlow | Year == 2018]
 
 # Sub only individuals with data in both seasons
-keepids <- sub[, .N, c(idcol, 'season')][!is.na(season), .N, idcol][N == 2][[idcol]]
-subSeasons <- sub[!is.na(season)][get(idcol) %chin% keepids]
-
+dropids <- c("FO2017011", "FO2016015", "FO2016009", "FO2018002",
+             "FO2016006", "FO2016008", "FO2016003", "FO2016011")
+sub <- sub[!get(idcol) %in% dropids]
+subseasons <- sub[!is.na(season)]
 
 ### Output ----
 saveRDS(sub, 'data/derived-data/sub-fogo-caribou.Rds')
-saveRDS(subSeasons, 'data/derived-data/sub-seasons-fogo-caribou.Rds')
+saveRDS(subseasons, 'data/derived-data/sub-seasons-fogo-caribou.Rds')
