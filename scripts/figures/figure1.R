2a2033d183376607465f3d9fac3307543e43a029
Quinn Webber <webber.quinn@gmail.com>
2020-01-27 16:48:49 -0330

    Update figure1.R

--- scripts/figures/figure1.R
+++ scripts/figures/figure1.R
@@ -5,26 +5,35 @@ library(ggforce)
 install.packages("remotes")
 remotes::install_github("sckott/rphylopic")
 
-df <- data.frame(x = c(4.25, 1, 0.5, 5), y = c(2.75, 1, 5.5, 0.5), 
+library(rphylopic)
+
+df <- data.frame(x = c(4.25, 1, 0.5, 2.5), y = c(2.75, 1, 5.5, 0.5), 
                  family = c("Association", "Aggression", "Vocal", "Scent"))
 
 ## select phylopic  
-person <- name_search(text = "Homo sapiens", options = "namebankID")[[1]]
+img <- rphylopic::image_data('f1b665ae-8fe9-42e4-b03a-4e9ae8213244', 512)[[1]]
+
 
 ggplot() +
-  geom_ellipse(aes(x0 = 4.25, y0 = 2.75, a = 5, b = 1.5, angle = 0.5), alpha = 0.25, fill = "blue") + ## assoc
-  geom_ellipse(aes(x0 = 1, y0 = 1, a = 1, b = 1, angle = 0), alpha = 0.25, fill = "red") + ## inter
-  geom_ellipse(aes(x0 = 0.5, y0 = 5.5, a = 0.5, b = 4, angle = 0), alpha = 0.25, fill = "green") + ## vocal
-  geom_ellipse(aes(x0 = 5, y0 = 0.5, a = 5, b = 0.25, angle = 0), alpha = 0.25, fill = "yellow") + ## scent
+  geom_ellipse(aes(x0 = 4.25, y0 = 2.75, a = 5, b = 1.5, angle = 0.5), alpha = 0.75, fill = "#d73027") + ## assoc
+  geom_ellipse(aes(x0 = 1, y0 = 1, a = 1.5, b = 1.5, angle = 0), alpha = 0.75, fill = "#fee090") + ## inter
+  geom_ellipse(aes(x0 = 0.5, y0 = 5.5, a = 0.75, b = 4, angle = 0), alpha = 0.75, fill = "#fc8d59") + ## vocal
+  geom_ellipse(aes(x0 = 2.5, y0 = 0.5, a = 3, b = 0.25, angle = 0), alpha = 0.75, fill = "#4575b4") + ## scent
   geom_text(data = df, aes(x, y, label = family)) +
-  ylab("Space") +
-  xlab("Time") +
-  #ylim(-0.5,10) +
-  #xlim(-0.5,10) +
+  add_phylopic(img, 1, y = 9, x = 9, ysize = 2) +
+  ylab("") +
+  xlab("") +
+  scale_x_continuous(limit = c(-1, 10), breaks=c(0, 2.5, 5, 7.5, 10), 
+                     labels= c("1 Second", "1 Minute",
+                             "1 Hour", "1 Day", "1 Month")) +
+  scale_y_continuous(limit = c(-1, 10), breaks=c(0, 2.5, 5, 7.5, 10), 
+                     labels= c("1m", "10m",
+                               "100m", "1000m", "10000m")) +
   coord_fixed() +
   theme(
     legend.position = 'none',
-    axis.text = element_blank(),
+    axis.text.x = element_text(size = 14, color = "black", angle = 45, vjust = 0.65),
+    axis.text.y = element_text(size = 14, color = "black"),
     axis.title = element_text(size = 18),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
