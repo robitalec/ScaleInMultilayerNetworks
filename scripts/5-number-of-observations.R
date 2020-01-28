035b16d9438d5516d25e341afc283a09f3e6e7c3
Alec Robitaille <robit.alec@gmail.com>
2020-01-28 19:00:57 -0330

    update 5- with lc*season

--- scripts/5-number-of-observations.R
+++ scripts/5-number-of-observations.R
@@ -37,11 +37,10 @@ randobs <- DT[, sample(unique(timegroup), size = maxn), season]
 var <- 'nobs'
 
 nets <- lapply(seq(10, maxn, by = nstep), function(n) {
-  col <- splitBy
-  
   # Select first n random timegroups, 
   #  adding new observations to the tail with each iteration
-  sub <- DT[timegroup %in% randobs[, .SD[1:n], season]$V1]
+  sub <- na.omit(DT, cols = splitBy)
+  sub <- sub[timegroup %in% randobs[, .SD[1:n], season]$V1]
 
   # Spatial grouping with spatsoc
   group_pts(
@@ -53,36 +52,33 @@ nets <- lapply(seq(10, maxn, by = nstep), function(n) {
     splitBy = splitBy
   )
   
-  usplit <- unique(na.omit(sub, cols = col)[[col]])
-  
   # GBI for each season
-  gbiLs <- list_gbi(sub, idcol, usplit, col)
+  gbiLs <- list_gbi(sub, idcol, splitBy)
   
   # Generate networks for each season
   netLs <- list_nets(gbiLs)
   
   # Generate graphs for each season
   gLs <- list_graphs(netLs)
-  names(gLs) <- paste0(n, '-', usplit)
+  names(gLs) <- names(gbiLs)
   
   # Calculate eigenvector centrality for each season
-  eig <- layer_eigen(gLs)
-  
-  # Calculate correlation of season layers
-  set(eig, j = 'layercorr', value = layer_correlation(gLs))
-  eig[, c(var, splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
-  setnames(eig, 'ind', idcol)
+  stren <- layer_strength(gLs)
+  stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
+  setnames(stren, 'ind', idcol)
   
   # Calculate neighbors
-  layer_neighbors(sub, idcol, splitBy = col)
+  layer_neighbors(sub, idcol, splitBy = splitBy)
   
   # and tidy output, prep for merge
-  outcols <- c('neigh', 'splitNeigh', idcol, col)
+  outcols <- c('neigh', 'splitNeigh', idcol, splitBy)
   out <- unique(sub[, .SD, .SDcols = outcols])
-  setnames(out, col, splitBy)
   
   # Merge eigcent+correlations with neighbors
-  out[eig, on = c(idcol, splitBy)]
+  out <- out[stren, on = c(idcol, splitBy)]
+  
+  # Preserve window length
+  set(out, j = var, value = n)
 })
 
 # TODO: check "found duplicate id in a timegroup and/or splitBy - does your group_times threshold match the fix rate?"
