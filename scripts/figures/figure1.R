a70f45a1d7d374a1f648af036c6c538c770021ac
Quinn Webber <webber.quinn@gmail.com>
2020-01-29 14:53:52 -0330

    fig1 done for now

--- scripts/figures/figure1.R
+++ scripts/figures/figure1.R
@@ -48,17 +48,18 @@ aa <- ggplot() +
                aes(x0 = x, y0 = y, a = 1.25, b = 3.25, angle = 0), 
                alpha = 0.75, fill = inter) +
   geom_ellipse(data = df[species == "hyeana" & behaviour == "Vocal"],
-               aes(x0 = x, y0 = y, a = 0.75, b = 13, angle = 0), 
+               aes(x0 = x, y0 = y, a = 0.75, b = 9.5, angle = 0), 
                alpha = 0.75, fill = voc) + 
   geom_ellipse(data = df[species == "hyeana" & behaviour == "Scent"],
                aes(x0 = x, y0 = y, a = 3, b = 1, angle = 0), 
                alpha = 0.75, fill = scent ) + 
   geom_ellipse(data = df[species == "hyeana" & behaviour == "Home range overlap"],
-               aes(x0 = x, y0 = y, a = 2.5, b = 16, angle = 0), 
+               aes(x0 = x, y0 = y, a = 2.5, b = 14, angle = 0), 
                alpha = 0.75, fill = hro) + 
-  geom_text(data = df[species == "hyeana"], aes(x, y, label = behaviour)) +
+  geom_text(data = df[species == "hyeana"], aes(x, y, label = behaviour),
+            fontface = "bold") +
   add_phylopic(hyeana, 1, y = 30, x = 1, ysize = ysize) +
-  ylab("Spatial scale") +
+  ylab("Relative spatial scale") +
   xlab("") +
   scale_x_continuous(limit = c(-1, 14), breaks=c(0, 2.5, 5, 7.5, 10, 12.5), 
                      labels= c("1 Second", "1 Minute",
@@ -82,9 +83,10 @@ bb <- ggplot() +
   geom_ellipse(data = df[species == "bird" & behaviour == "Home range overlap"],
                aes(x0 = x, y0 = y, a = 3, b = 7, angle = 0), 
                alpha = 0.75, fill = hro) + 
-  geom_text(data = df[species == "bird"], aes(x, y, label = behaviour)) +
+  geom_text(data = df[species == "bird"], aes(x, y, label = behaviour),
+            fontface = "bold") +
   add_phylopic(bird, 1, y = 30, x = 1, ysize = ysize) +
-  ylab("Spatial scale") +
+  ylab("") +
   xlab("") +
   scale_x_continuous(limit = c(-1, 14), breaks=c(0, 2.5, 5, 7.5, 10, 12.5), 
                      labels= c("1 Second", "1 Minute",
@@ -108,10 +110,11 @@ cc <- ggplot() +
   geom_ellipse(data = df[species == "lizard" & behaviour == "Home range overlap"],
                aes(x0 = x, y0 = y, a = 2.5, b = 11.5, angle = 0), 
                alpha = 0.75, fill = hro) + 
-  geom_text(data = df[species == "lizard"], aes(x, y, label = behaviour)) +
+  geom_text(data = df[species == "lizard"], aes(x, y, label = behaviour),
+            fontface = "bold") +
   add_phylopic(lizard, 1, y = 30, x = 1, ysize = 4) +
-  ylab("Spatial scale") +
-  xlab("") +
+  ylab("Relative spatial scale") +
+  xlab("Relative time scale") +
   scale_x_continuous(limit = c(-1, 14), breaks=c(0, 2.5, 5, 7.5, 10, 12.5), 
                      labels= c("1 Second", "1 Minute",
                                "1 Hour", "1 Day", "1 Month", "1 Year")) +
@@ -123,24 +126,25 @@ cc <- ggplot() +
 
 dd <- ggplot() +
   geom_ellipse(data = df[species == "elephant" & behaviour == "Association"], 
-               aes(x0 = x, y0 = y, a = 4, b = 1.5, angle = 0.5), 
+               aes(x0 = x, y0 = y, a = 5.5, b = 4, angle = 0.75), 
                alpha = 0.75, fill = assoc) +  
   geom_ellipse(data = df[species == "elephant" & behaviour == "Interaction"],
-               aes(x0 = x, y0 = y, a = 1.5, b = 1.5, angle = 0), 
+               aes(x0 = x, y0 = y, a = 1.5, b = 3.25, angle = 0), 
                alpha = 0.75, fill = inter) + 
   geom_ellipse(data = df[species == "elephant" & behaviour == "Scent"],
-               aes(x0 = x, y0 = y, a = 4, b = 0.75, angle = 0), 
+               aes(x0 = x, y0 = y, a = 3, b = 2, angle = 0), 
                alpha = 0.75, fill = scent) + 
   geom_ellipse(data = df[species == "elephant" & behaviour == "Home range overlap"],
-               aes(x0 = x, y0 = y, a = 3, b = 5, angle = 0.1), 
+               aes(x0 = x, y0 = y, a = 2.5, b = 16, angle = 0), 
                alpha = 0.75, fill = hro) + 
   geom_ellipse(data = df[species == "elephant" & behaviour == "Vocal"],
-               aes(x0 = x, y0 = y, a = 2, b = 5, angle = 0.1), 
+               aes(x0 = x, y0 = y, a = 0.75, b = 14, angle = 0), 
                alpha = 0.75, fill = voc) + 
-  geom_text(data = df[species == "elephant"], aes(x, y, label = behaviour)) +
+  geom_text(data = df[species == "elephant"], aes(x, y, label = behaviour),
+            fontface = "bold") +
   add_phylopic(elephant, 1, y = 30, x = 1, ysize = 7) +
-  ylab("Spatial scale") +
-  xlab("") +
+  ylab("") +
+  xlab("Relative time scale") +
   scale_x_continuous(limit = c(-1, 14), breaks=c(0, 2.5, 5, 7.5, 10, 12.5), 
                      labels= c("1 Second", "1 Minute",
                                "1 Hour", "1 Day", "1 Month", "1 Year")) +
