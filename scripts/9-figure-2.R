27c7905c9edd8532a6ee55349b051de60f1a9ad2
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-30 23:57:26 -0400

    by season meanX meanY

--- scripts/9-figure-2.R
+++ scripts/9-figure-2.R
@@ -56,7 +56,7 @@ gLs <- list_graphs(netLs)
 names(gLs) <- names(gbiLs)
 
 # Restructure for plotting
-meanXY <- DT[, .(meanX = mean(get(xcol)), meanY = mean(get(ycol))), by = idcol]
+meanXY <- DT[, .(meanX = mean(get(xcol)), meanY = mean(get(ycol))), by = .(season, idcol)]
 
 edges <- rbindlist(lapply(gLs, as_data_frame, what = 'edges'), idcol = 'layer')
 
@@ -79,25 +79,26 @@ m[, c(lccol, 'season') := tstrsplit(layer, '-', type.convert = TRUE)]
   xend = meanXend,
   yend = meanYend
 )) +
-  geom_edges(aes(size = weight), alpha = 0.2) +
+  geom_edges(size = 1, alpha = 0.6) +
   geom_nodes(aes(color = from), size = 7) +
   # geom_nodetext(aes(label = from)) +
   # scale_color_manual(breaks = DT$ID, values = pal) +
-  facet_wrap(~lc30 + season) + 
-  guides(color = FALSE) +
-  theme(axis.line = element_blank(),
-        panel.border = element_rect(fill = NA)))
-
+  facet_wrap(~season + lc30, dir = 'h') + 
+  # guides(color = FALSE) + 
+    scale_color_grey()#+
+  # theme(axis.line = element_blank(),
+  #       panel.border = element_rect(fill = NA)))
+)
 
 
 ### Set theme ----
-# pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442")
-# 
-# theme_set(theme_classic())
-# theme_update(axis.text = element_blank(),
-#              axis.title = element_blank(),
-#              axis.ticks = element_blank(),
-#              aspect.ratio = 1)
+pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442")
+
+theme_set(theme_classic())
+theme_update(axis.text = element_blank(),
+             axis.title = element_blank(),
+             axis.ticks = element_blank(),
+             aspect.ratio = 1)
 # 
 # fontSize <- 24
 # gridTheme <- gridExtra::ttheme_default(
