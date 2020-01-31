163714a7e7d1ad4bc4069e96341ad073886bbb00
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-30 23:18:24 -0400

    set up each network

--- scripts/9-figure-2.R
+++ scripts/9-figure-2.R
@@ -9,7 +9,10 @@ pkgs <- c('data.table',
           'rgdal',
           'asnipe',
           'igraph', 
-          'ScaleInMultilayerNetworks')
+          'ScaleInMultilayerNetworks',
+          # 'patchwork',
+          # 'ggthemes',
+          'ggnetwork')
 p <- lapply(pkgs, library, character.only = TRUE)
 
 
@@ -28,61 +31,29 @@ group_times(
   threshold = '10 minutes'
 )
 
-### Generate networks for each spatial threshold ----
-# list spatial thresholds
-thresholds <- c(5, seq(50, 500, by = 50))
-
-var <- 'spatialthreshold'
-
-
-nets <- lapply(thresholds, function(t) {
-  sub <- na.omit(DT, cols = splitBy)
+# Spatial grouping with spatsoc
+group_pts(
+  DT,
+  threshold = spatthresh,
+  id = idcol,
+  coords = projCols,
+  timegroup = 'timegroup',
+  splitBy = splitBy
+)
   
-  # Spatial grouping with spatsoc
-  group_pts(
-    sub,
-    threshold = t,
-    id = idcol,
-    coords = projCols,
-    timegroup = 'timegroup',
-    splitBy = splitBy
-  )
   
-  # GBI for each season
-  gbiLs <- list_gbi(sub, idcol, splitBy)
+# GBI 
+gbiLs <- list_gbi(DT, idcol, splitBy)
   
-  # Generate networks for each season
-  netLs <- list_nets(gbiLs)
+# Networks
+netLs <- list_nets(gbiLs)
   
-  # Generate graphs for each season
-  gLs <- list_graphs(netLs)
-  names(gLs) <- names(gbiLs)
+# Graphs
+gLs <- list_graphs(netLs)
+names(gLs) <- names(gbiLs)
   
-  # Calculate eigenvector centrality for each season
-  stren <- layer_strength(gLs)
-  stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
-  setnames(stren, 'ind', idcol)
-  
-  # Calculate neighbors
-  layer_neighbors(sub, idcol, splitBy = splitBy)
-  
-  # and tidy output, prep for merge
-  outcols <- c('neigh', 'splitNeigh', idcol, splitBy)
-  out <- unique(sub[, .SD, .SDcols = outcols])
-  
-  # Merge eigcent+correlations with neighbors
-  out <- out[stren, on = c(idcol, splitBy)]
-  
-  # Preserve window length
-  set(out, j = var, value = t)
-})
-
-out <- rbindlist(nets)
 
 
-### Output ----
-saveRDS(out, 'data/derived-data/3-spatial-threshold.Rds')
-### Figure 5 - edges ====
 
 ### Packages ----
 pkgs <- c(
