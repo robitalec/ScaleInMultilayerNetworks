002c391fea8a19f4b46e39411123ee5608670b27
Alec L. Robitaille <robit.alec@gmail.com>
2020-01-30 23:15:46 -0400

    fst ml net fig script

---
+++ scripts/9-figure-2.R
@@ -0,0 +1,217 @@
+# Figure 2
+
+### Scale in multilayer networks - spatial threshold
+# Alec Robitaille
+
+### Packages ----
+pkgs <- c('data.table',
+          'spatsoc',
+          'rgdal',
+          'asnipe',
+          'igraph', 
+          'ScaleInMultilayerNetworks')
+p <- lapply(pkgs, library, character.only = TRUE)
+
+
+### Variables ----
+source('scripts/0-variables.R')
+
+
+### Data ----
+DT <- readRDS('data/derived-data/1-sub-seasons-fogo-caribou.Rds')
+alloc.col(DT)
+
+### Temporal grouping with spatsoc ----
+group_times(
+  DT,
+  datetime =  c(datecol, timecol),
+  threshold = '10 minutes'
+)
+
+### Generate networks for each spatial threshold ----
+# list spatial thresholds
+thresholds <- c(5, seq(50, 500, by = 50))
+
+var <- 'spatialthreshold'
+
+
+nets <- lapply(thresholds, function(t) {
+  sub <- na.omit(DT, cols = splitBy)
+  
+  # Spatial grouping with spatsoc
+  group_pts(
+    sub,
+    threshold = t,
+    id = idcol,
+    coords = projCols,
+    timegroup = 'timegroup',
+    splitBy = splitBy
+  )
+  
+  # GBI for each season
+  gbiLs <- list_gbi(sub, idcol, splitBy)
+  
+  # Generate networks for each season
+  netLs <- list_nets(gbiLs)
+  
+  # Generate graphs for each season
+  gLs <- list_graphs(netLs)
+  names(gLs) <- names(gbiLs)
+  
+  # Calculate eigenvector centrality for each season
+  stren <- layer_strength(gLs)
+  stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
+  setnames(stren, 'ind', idcol)
+  
+  # Calculate neighbors
+  layer_neighbors(sub, idcol, splitBy = splitBy)
+  
+  # and tidy output, prep for merge
+  outcols <- c('neigh', 'splitNeigh', idcol, splitBy)
+  out <- unique(sub[, .SD, .SDcols = outcols])
+  
+  # Merge eigcent+correlations with neighbors
+  out <- out[stren, on = c(idcol, splitBy)]
+  
+  # Preserve window length
+  set(out, j = var, value = t)
+})
+
+out <- rbindlist(nets)
+
+
+### Output ----
+saveRDS(out, 'data/derived-data/3-spatial-threshold.Rds')
+### Figure 5 - edges ====
+
+### Packages ----
+pkgs <- c(
+  'data.table',
+  'ggplot2',
+  'patchwork',
+  'ggthemes',
+  'spatsoc',
+  'igraph',
+  'ggnetwork',
+  'gridExtra'
+)
+p <- lapply(pkgs, library, character.only = TRUE)
+
+### Import data ----
+DT <- readRDS('data/example-pts.Rds')
+
+DT <- DT[timegroup == 2]
+
+DT[ID == 'H', c('X', 'Y') := .(700445, 5508555)]
+
+### Set theme ----
+pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442")
+
+theme_set(theme_classic())
+theme_update(axis.text = element_blank(),
+             axis.title = element_blank(),
+             axis.ticks = element_blank(),
+             aspect.ratio = 1)
+
+fontSize <- 24
+gridTheme <- gridExtra::ttheme_default(
+  base_size = fontSize
+)
+
+### Distance matrix ----
+distm <- DT[, as.matrix(dist(cbind(X, Y)))]
+distm <- round(distm, digits = 2)
+
+rownames(distm) <- unique(DT$ID)
+colnames(distm) <- unique(DT$ID)
+
+m <- melt(distm < 50)
+setDT(m)
+m <- m[(value) & Var1 != Var2, .(Var1, Var2)]
+setnames(m, c('ID1', 'ID2'))
+
+## Edge dist ----
+setnames(DT, 'ID', 'ID1')
+m[DT, c('X', 'Y') := .(X, Y), on = 'ID1']
+m <- merge(m,
+           DT[, .(ID1, X, Y)],
+           by.x = 'ID2',
+           by.y = 'ID1',
+           suffixes = c('', 'end'))
+m <- rbind(m, DT[ID1 == 'H', .(ID1, X, Y)], fill = TRUE)
+
+
+edgedist <-
+  ggplot() +
+  annotation_custom(tableGrob(m[order(ID1), .(ID1, ID2)], rows = NULL))
+
+
+
+gdist <- ggplot(m, aes(
+  x = X,
+  y = Y,
+  xend = Xend,
+  yend = Yend
+)) +
+  geom_edges() +
+  geom_nodes(aes(color = ID1), size = 7) +
+  geom_nodetext(aes(label = ID1)) +
+  scale_color_manual(breaks = DT$ID1, values = pal) +
+  guides(color = FALSE) +
+  theme(axis.line = element_blank(),
+        panel.border = element_rect(fill = NA))
+
+
+### Edge nn ----
+m <- melt(distm)
+setDT(m)
+m <-
+  m[value != 0][, .SD[value == min(value)], Var1][order(value), .(Var1, Var2)]
+setnames(m, c('ID', 'NN'))
+setnames(DT, 'ID1', 'ID')
+m[DT, c('X', 'Y') := .(X, Y), on = 'ID']
+m <- merge(m,
+           DT[, .(ID, X, Y)],
+           by.x = 'NN',
+           by.y = 'ID',
+           suffixes = c('', 'end'))
+
+
+edgenn <-
+  ggplot() +
+  annotation_custom(tableGrob(m[order(ID), .(ID, NN)], rows = NULL))
+
+gnn <- ggplot(m, aes(
+  x = X,
+  y = Y,
+  xend = Xend,
+  yend = Yend
+)) +
+  geom_edges() +
+  geom_nodes(aes(color = ID), size = 7) +
+  geom_nodetext(aes(label = ID)) +
+  scale_color_manual(breaks = DT$ID, values = pal) +
+  guides(color = FALSE) +
+  theme(axis.line = element_blank(),
+        panel.border = element_rect(fill = NA))
+
+### Output ----
+fig5 <- gdist + edgedist + gnn + edgenn +
+  plot_layout(ncol = 2, widths = c(3.4, 1)) +
+  plot_annotation(tag_levels = 'A') &
+  theme(
+    plot.tag = element_text(size = 14, face = 2),
+    legend.position = c(.9, .75),
+    legend.text = element_text(size = 16, face = 1),
+    legend.title = element_text(size = 16, face = 1)
+  )
+
+ggsave(
+  filename = 'figures/Figure5.pdf',
+  plot = fig5,
+  width = 115,
+  height = 145,
+  units = 'mm'
+)
+
+
