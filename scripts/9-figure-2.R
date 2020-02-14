324f8c83b0dd201d4d8205f303661b0e53c24e2d
Alec L. Robitaille <robit.alec@gmail.com>
2020-02-13 20:49:05 -0400

    scale color viridis

--- scripts/9-figure-2.R
+++ scripts/9-figure-2.R
@@ -159,12 +159,13 @@ labels <- data.table(
                    size = weight),
                alpha = 1
     ) +
-    scale_size(range=c(0.1, 2)) + 
+    scale_size(range = c(0.1, 2)) + 
     # geom_edges(aes(group = layer,
     #                xend = shearxend + modx,
     #                yend = shearyend + mody
     # ), data = zzz[, .SD[order(vertex.names, modx, mody)]]) +
     geom_nodes(aes(color = vertex.names), size = 5) +
+    scale_color_viridis_d() + 
     guides(color = FALSE) +
     geom_text(aes(x, y, xend = NULL, yend = NULL, label = label), data = labels) + 
   p
