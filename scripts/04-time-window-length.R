d5fa44f5a6126e6adcd6de6a7d33f3d5c83b689c
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-29 14:37:32 -0230

    start adjust sensitivity -> ordinal

--- scripts/04-time-window-length.R
+++ scripts/04-time-window-length.R
@@ -24,10 +24,6 @@ alloc.col(DT)
 
 
 # Chunk time --------------------------------------------------------------
-# Remove season column from 1-data-prep.R
-DT[, season := NULL]
-
-
 nchunk <- 8
 
 # TODO: check consistent N by cut * year
@@ -44,54 +40,49 @@ group_times(
 
 
 # Generate network for each time chunk ------------------------------------
-var <- 'winlength'
+var <- 'timecut'
+splitBy <- var
 
-nets <- lapply(winlengths, function(len) {
-  col <- paste0('season', len)
-  splitBy <- c(col, lccol)
-  
-  sub <- na.omit(DT, cols = splitBy)
-  
-  # Spatial grouping with spatsoc
-  group_pts(
-    sub,
-    threshold = spatthresh,
-    id = idcol,
-    coords = projCols,
-    timegroup = 'timegroup',
-    splitBy = splitBy
-  )
+# Spatial grouping with spatsoc
+# TODO: double fixes
+group_pts(
+  DT,
+  threshold = spatthresh,
+  id = idcol,
+  coords = projCols,
+  timegroup = 'timegroup',
+  splitBy = splitBy
+)
   
   
-  # GBI 
-  gbiLs <- list_gbi(sub, idcol, splitBy)
+# GBI
+gbiLs <- list_gbi(DT, idcol, splitBy)
 
-  # Generate networks
-  netLs <- list_nets(gbiLs)
-  
-  # Generate graphs
-  gLs <- list_graphs(netLs)
-  names(gLs) <- names(gbiLs)
-  
-  # Calculate eigenvector centrality
-  stren <- layer_strength(gLs)
-  stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
-  setnames(stren, 'ind', idcol)
-  
-  # Calculate neighbors
-  layer_neighbors(sub, idcol, splitBy = splitBy)
+# Generate networks
+netLs <- list_nets(gbiLs)
 
-  # and tidy output, prep for merge
-  outcols <- c('neigh', 'splitNeigh', idcol, splitBy)
-  out <- unique(sub[, .SD, .SDcols = outcols])
-  
-  # Merge eigcent+correlations with neighbors
-  out <- out[stren, on = c(idcol, splitBy)]
-  setnames(out, col, gsub('[0-9]', '', col))
-  
-  # Preserve window length
-  set(out, j = var, value = len)
-})
+# Generate graphs
+gLs <- list_graphs(netLs)
+names(gLs) <- names(gbiLs)
+
+# Calculate eigenvector centrality
+stren <- layer_strength(gLs)
+stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
+setnames(stren, 'ind', idcol)
+
+# Calculate neighbors
+layer_neighbors(sub, idcol, splitBy = splitBy)
+
+# and tidy output, prep for merge
+outcols <- c('neigh', 'splitNeigh', idcol, splitBy)
+out <- unique(sub[, .SD, .SDcols = outcols])
+
+# Merge eigcent+correlations with neighbors
+out <- out[stren, on = c(idcol, splitBy)]
+setnames(out, col, gsub('[0-9]', '', col))
+
+# Preserve window length
+set(out, j = var, value = len)
 
 out <- rbindlist(nets)
 
