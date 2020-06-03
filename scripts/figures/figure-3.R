8533fd0edaac25a15a9cd60c524ef1f46b69b2dc
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-03 18:55:47 -0230

    fst fill fig 3

--- scripts/figures/figure-3.R
+++ scripts/figures/figure-3.R
@@ -1,9 +1,33 @@
-  saveRDS(out, 'data/derived-data/2-landcover-scale.Rds')
-saveRDS(count, 'data/derived-data/2-landcover-scale-count.Rds')
-ggplot(count[lc %in% c(1, 2, 3)]) + geom_line(aes(res, N, color = factor(lc), group = lc))
+# === Figure 3 ------------------------------------------------------------
+# Alec Robitaille
 
 
-ggplot(out) + geom_line(aes(lcres, propedges, color = layer))
+# Packages ----------------------------------------------------------------
+pkgs <- c('data.table', 'ggplot2', 'patchwork')
+p <- lapply(pkgs, library, character.only = TRUE)
 
 
-ggplot(out) + geom_line(aes(lcres, graphstrength, color = ANIMAL_ID)) + facet_wrap(~layer) + guides(color = FALSE)
+# Input -------------------------------------------------------------------
+DT <- readRDS('data/derived-data/2-landcover-scale.Rds')
+count <- readRDS('data/derived-data/2-landcover-scale-count.Rds')
+
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(count[lc %in% c(1, 2, 3)]) + 
+  geom_line(aes(res, N, color = factor(lc), group = lc))
+
+ggplot(out) + 
+  geom_line(aes(lcres, propedges, color = layer))
+
+
+ggplot(out) + geom_line(aes(lcres, graphstrength, color = ANIMAL_ID)) + 
+  facet_wrap(~ layer) + 
+  guides(color = FALSE)
+
+
+# Output ------------------------------------------------------------------
+
+
+
