87bd73c25dc1976ce42996d6a9eae99ae2807469
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-03 19:19:37 -0230

    add legend

--- scripts/figures/figure-3.R
+++ scripts/figures/figure-3.R
@@ -12,17 +12,30 @@ DT <- readRDS('data/derived-data/2-landcover-scale.Rds')
 count <- readRDS('data/derived-data/2-landcover-scale-count.Rds')
 
 
+legend <- data.table(lc = c(1, 2, 3),
+                     lcname = c('open', 'forest', 'forage'))
+
+DT[legend, lcname := lcname, on = 'lc']
+count[legend, lcname := lcname, on = 'lc']
+
+
+
+# Theme -------------------------------------------------------------------
+source('scripts/figures/theme.R')
 
 # Plot --------------------------------------------------------------------
 
+count[lc %in% c(1, 2, 3), percent := N / sum(N), by = res]
+
 ggplot(count[lc %in% c(1, 2, 3)]) + 
-  geom_line(aes(res, N, color = factor(lc), group = lc))
-
-ggplot(out) + 
-  geom_line(aes(lcres, propedges, color = layer))
+  geom_line(aes(res, percent, color = factor(lcname), group = lcname))
 
 
-ggplot(out) + geom_line(aes(lcres, graphstrength, color = ANIMAL_ID)) + 
+ggplot(DT) + 
+  geom_line(aes(lcres, propedges, color = lcname))
+
+
+ggplot(DT) + geom_line(aes(lcres, graphstrength, color = ANIMAL_ID)) + 
   facet_wrap(~ layer) + 
   guides(color = FALSE)
 
