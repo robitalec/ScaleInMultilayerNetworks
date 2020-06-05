21a365b3b12ad7e3c5b9ccb7007829449922813a
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-05 00:50:14 -0230

    plot rasters in fig 3?

--- scripts/figures/figure-3.R
+++ scripts/figures/figure-3.R
@@ -4,7 +4,7 @@
 
 
 # Packages ----------------------------------------------------------------
-pkgs <- c('data.table', 'ggplot2', 'patchwork')
+pkgs <- c('data.table', 'ggplot2', 'patchwork', 'raster')
 p <- lapply(pkgs, library, character.only = TRUE)
 
 
@@ -28,23 +28,24 @@ source('scripts/figures/theme.R')
 count[lc %in% c(1, 2, 3), 
       percent := N / sum(N) * 100, by = res]
 
-xlab <- 'Land Cover Resolution'
-
-ggplot(count[lc %in% c(1, 2, 3)]) + 
-  geom_line(aes(res, percent, 
-                linetype = lcname)) +
-  scale_linetype_manual(values = linetypes)
-
-
-ggplot(DT) + 
-  geom_line(aes(lcres, propedges, 
-                linetype = lcname)) +
-  scale_linetype_manual(values = linetypes)
-  
-
-
 DT[, meangraphstrength := mean(graphstrength, na.rm = TRUE),
    by = .(lcname, lcres)]
+
+
+xlab <- 'Land Cover Resolution'
+
+
+
+gcount <- ggplot(count[lc %in% c(1, 2, 3)]) + 
+  geom_line(aes(res, percent, 
+                linetype = lcname))
+
+
+gprop <- ggplot(DT) + 
+  geom_line(aes(lcres, propedges, 
+                linetype = lcname)) + 
+  guides(linetype = FALSE)
+  
 
 gstr <- ggplot(DT) +
   geom_line(aes(lcres, graphstrength, group = ANIMAL_ID),
@@ -55,8 +56,34 @@ gstr <- ggplot(DT) +
   guides(color = FALSE)
 
 
+layout <- 'AAA#
+           BBBD
+           CCC#'
+
+(gcount / 
+  gstr + guide_area() / 
+  gprop & 
+   scale_linetype_manual(values = linetypes) &
+   labs(x = xlab)) +
+  plot_layout(design = layout,
+              guides = 'collect')
 
 
+lc30 <- readRDS('data/derived-data/1-reclass-lc.Rds')
+lc500 <- raster('data/derived-data/2-landcover-res-500.tif')
+lc1000 <- raster('data/derived-data/2-landcover-res-1000.tif')
+
+rasterVis::gplot(lc30) + 
+  geom_tile(aes(fill = factor(value))) + 
+  scale_fill_manual(values = greys)
+
+rasterVis::gplot(lc500) + 
+  geom_tile(aes(fill = factor(value))) + 
+  scale_fill_manual(values = greys)
+
+rasterVis::gplot(lc1000) + 
+  geom_tile(aes(fill = factor(value))) + 
+  scale_fill_manual(values = greys)
 
 # Output ------------------------------------------------------------------
 
