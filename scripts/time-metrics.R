fc0577fabbdfc7a9eb9e0c72b09839a1eab3a22f
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-07 15:01:51 -0230

    adjust plots w/i time metrics

--- scripts/time-metrics.R
+++ scripts/time-metrics.R
@@ -29,17 +29,14 @@ layer_similarity_ordinal(matrices, 'FO', var)
 library(ggplot2)
 
 
-melt(matrices, id.vars = grep('FO', colnames(matrices)),
-     measure.vars = c('layersim',))
+matrices[DT, c('mindate', 'maxdate') := .(mindate, maxdate), on = 'timecut']
 
-matrices[DT, c('cutJDate', 'Year') := .(cutJDate, Year), on = 'timecut']
 ggplot(matrices) + 
   geom_line(aes(timecut, layersim))
-ggplot(matrices) + 
-  geom_line(aes(cutJDate, layersim, group  =1))
-ggplot(matrices) + 
-  geom_line(aes(layer, layersim))
 
+ggplot(matrices) + 
+  geom_segment(aes(x = mindate, xend = maxdate,
+                   y = layersim, yend = layersim))
 
 DT[matrices, layersim := layersim, on = 'layer']
 
@@ -48,6 +45,7 @@ multi_degree(DT, 'splitNeigh', idcol, splitBy = var)
 
 # Redundancy
 connective_redudancy(DT)
+# TODO not true
 stopifnot(DT[!between(connredund, 0, 1), .N] == 0)
 
 
@@ -58,11 +56,12 @@ deviation_degree(DT, 'splitNeigh', idcol, splitBy = var)
 layer_relevance(DT, idcol, splitBy = splitBy)
 stopifnot(DT[!between(relev, 0, 1), .N] == 0)
 
-ggplot(DT) + geom_line(aes(cutJDate, relev, group = ANIMAL_ID))
+ggplot(DT) + 
+  geom_line(aes(mindate, relev, color = ANIMAL_ID)) +
+  guides(color = FALSE)
 
 # Mean across individuals (for plots) -------------------------------------
 # TODO: check light grey, with mean on top
-splitBy <- c('season', lccol)
 
 # Across layers - degree deviation, neigh, multideg, connredund
 metriccols <- c('neigh', 'multideg', 'degdev', 'connredund')
