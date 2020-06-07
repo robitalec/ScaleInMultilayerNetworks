3578cf6b64b209e13d9a59470778b79c3b87666d
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-07 01:54:58 -0230

    fix calc min max date

--- scripts/03-temporal-layers.R
+++ scripts/03-temporal-layers.R
@@ -33,8 +33,9 @@ DT[, dayoffull := .GRP, .(JDate, Year)]
 DT[, cutJDate := cut(dayoffull, nchunk, include.lowest = TRUE)]
 DT[, timecut := .GRP, cutJDate]
 
-DT[, mindate := min(as.IDate(datetime)), by = timecut]
-DT[, maxdate := max(mindate), by = timecut]
+DT[, date := as.IDate(datetime)]
+DT[, mindate := min(date), by = timecut]
+DT[, maxdate := max(date), by = timecut]
 
 # Temporal grouping with spatsoc ------------------------------------------
 group_times(
