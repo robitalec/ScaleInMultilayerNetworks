956382dd65975c0b56c3a3510704b010586cdd66
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-09 20:25:49 -0230

    alternative palette options

--- scripts/figures/theme.R
+++ scripts/figures/theme.R
@@ -6,22 +6,22 @@
 # Theme -------------------------------------------------------------------
 base <- 
   theme(
-    legend.text = element_text(size = 12, color = "black"),
+    legend.text = element_text(size = 12, color = 'black'),
     legend.title = element_blank(),
     legend.spacing = unit(-0.5, 'cm'),
     legend.background = element_blank(),
     legend.key = element_blank(),
     axis.text.x = element_text(
       size = 12,
-      color = "black",
+      color = 'black',
       vjust = 0.65
     ),
-    axis.text.y = element_text(size = 14, color = "black"),
+    axis.text.y = element_text(size = 14, color = 'black'),
     axis.title = element_text(size = 18),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
     panel.border = element_rect(
-      colour = "black",
+      colour = 'black',
       fill = NA,
       size = 1
     )
@@ -36,11 +36,12 @@ linesize <- 1.3
 
 
 # Colors ------------------------------------------------------------------
-lccolors <- c("Open" = "#fdc086",
-              "Forest" = "#7fc97f",
-              "Forage" = "#beaed4")
+lccolors <- c('#fdc086', '#7fc97f', '#beaed4')
+# lccolors <- scales::viridis_pal(alpha = 1, option = 'E')(5)[c(1, 3, 4)]
+  # scales::viridis_pal(alpha = 0.7)(4)[1:3]
+names(lccolors) <- c('Open', 'Forest', 'Forage')
 lcvalcolors <- lccolors
 names(lcvalcolors) <-c(1, 2, 3)
 
 cols <- c('winter' = '#8c510a', 'summer' = '#5ab4ac')
-greys <- c(`1` = "#6b6b6b", `2` = "#c2c2c2", `3` = "#919191")
+greys <- c(`1` = '#6b6b6b', `2` = '#c2c2c2', `3` = '#919191')
