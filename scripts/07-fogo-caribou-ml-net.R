d00a5183eefd26f1acc1db80705c02f2d734e5bb
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-10 02:48:00 -0230

    add grouping/graphs from temp

--- scripts/07-fogo-caribou-ml-net.R
+++ scripts/07-fogo-caribou-ml-net.R
@@ -41,10 +41,87 @@ DT[, nid := uniqueN(ANIMAL_ID), timecut]
 
 
 # Group chunks
-DT[timecut == 1, season := 'drop']
+# drop timecut 1
+DT <- DT[timecut != 1]
+
 DT[timecut %in% 2:5, season := 1]
 DT[timecut %in% 6:11, season := 2]
 DT[timecut %in% 12:15, season := 3]
 DT[timecut %in% 16:20, season := 4]
 
+DT[season == 1, #c('seasonstart', 'seasonend', 'seasonnm') := 
+     c(range(datetime), 'May 2017 - October - 2017')]
+
+DT[timecut %in% 6:11, season := 2]
+DT[timecut %in% 12:15, season := 3]
+DT[timecut %in% 16:20, season := 4]
+
 #TODO: set years
+
+# Temporal grouping with spatsoc ------------------------------------------
+group_times(
+  DT,
+  datetime =  c(datecol, timecol),
+  threshold = tempthresh
+)
+
+
+# Generate network for each time chunk ------------------------------------
+var <- 'season'
+splitBy <- var
+
+# Spatial grouping with spatsoc
+group_pts(
+  DT,
+  threshold = spatthresh,
+  id = idcol,
+  coords = projCols,
+  timegroup = 'timegroup',
+  splitBy = splitBy
+)
+
+
+# GBI
+gbiLs <- list_gbi(DT, idcol, splitBy)
+
+# Generate networks
+netLs <- list_nets(gbiLs)
+
+# Generate graphs
+gLs <- list_graphs(netLs)
+names(gLs) <- names(gbiLs)
+
+# Generate edge lists
+eLs <- list_edges(gLs)
+
+# Calculate edge overlap
+eovr <- edge_overlap(eLs)
+eovr[, layer := as.integer(layer)]
+eovr[, edgeoverlapmat := list(edge_overlap_mat(eLs))]
+
+# Calculate eigenvector centrality
+stren <- layer_strength(gLs)
+stren[, layer := as.integer(layer)]
+stren[, (splitBy) := tstrsplit(layer, '-', type.convert = TRUE)]
+setnames(stren, 'ind', idcol)
+
+# Calculate neighbors
+layer_neighbors(DT, idcol, splitBy = splitBy)
+
+# and tidy output, prep for merge
+outcols <- c('neigh', 'splitNeigh', idcol, 'cutJDate', 
+             splitBy, 'mindate', 'maxdate', 'nid')
+usub <- unique(DT[, .SD, .SDcols = outcols])
+
+# Merge eigcent+correlations with neighbors
+wstren <- usub[stren, on = c(idcol, splitBy)]
+
+# Merge edge overlap
+wedgeovr <- wstren[eovr, on = 'layer']
+
+# Property matrix
+matrices <- property_matrix(wedgeovr, idcol, 'splitNeigh', var)
+layer_similarity_ordinal(matrices, 'FO', var)
+
+out <- wedgeovr[matrices[, .(layersim, layer)], on = 'layer']
+
