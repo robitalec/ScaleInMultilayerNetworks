c2915fb824442f780b8fa9516741d935a5e5e3ca
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-16 17:41:02 -0230

    fill plan with all scale and figures

--- scripts/plan.R
+++ scripts/plan.R
@@ -9,14 +9,55 @@ source('scripts/00-variables.R')
 
 # Setup scripts as functions ----------------------------------------------
 data_prep <- code_to_function('scripts/01-data-prep.R')
-landcover_scale <- code_to_function('scripts/02-landcover-scale.R')
-figure_lc <- code_to_function('scripts/figures/figure-lc.R')
 
+# Figure 1
+fig_1 <- code_to_function('scripts/figures/figure-scales.R')
+
+# Land Cover
+scale_lc <- code_to_function('scripts/02-landcover-scale.R')
+fig_lc <- code_to_function('scripts/figures/figure-lc.R')
+
+# Temporal
+scale_temp <- code_to_function('scripts/03-temporal-layers.R')
+fig_temp <- code_to_function('scripts/figures/figure-temp.R')
+
+# Social Threshold
+scale_soc <- code_to_function('scripts/04-social-distance-threshold.R')
+fig_soc <- code_to_function('scripts/figures/figure-lc.R')
+
+# Number of Observations
+scale_nobs <- code_to_function('scripts/05-number-of-observations.R')
+fig_nobs <- code_to_function('scripts/figures/figure-nobs.R')
+
+
+# TODO: fogo car ml
 
 
 # Plan --------------------------------------------------------------------
 plan <- drake_plan(
+  
+  # Prep data
   data = data_prep,
-  lc = landcover_scale(data),
-  fig_lc = figure_lc(lc)
+  
+  # Figure 1 
+  figure1 = figure_1(), 
+  
+  # Land cover
+  lc = lc_scale(data),
+  lc_figure = lc_fig(lc),
+  
+  # Temporal
+  temp = temp_scale(data),
+  temp_figure = temp_fig(temp),
+  
+  # Social threshold
+  soc = soc_scale(data),
+  soc_figure = soc_fig(soc),
+  
+
+  # Number of observations
+  nobs = nobs_scale(data),
+  nobs_figure = nobs_fig(nobs)
+  
+  # TODO: fogo car ml
 )
