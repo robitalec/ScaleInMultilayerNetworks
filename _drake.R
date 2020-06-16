a2b4386f1e7bdf53c8a63f035d001dd5cdca748f
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-16 13:37:57 -0230

    adjust _drake.R

--- _drake.R
+++ _drake.R
@@ -1,25 +1,19 @@
-# Try drake
-
-# install.packages('drake')
-
+# Load drake
 library(drake)
 
+# Source universal variables and figure theme
 source('scripts/figures/theme.R')
 source('scripts/00-variables.R')
 
+# Setup scripts as functions
 data_prep <- code_to_function('scripts/01-data-prep.R')
 landcover_scale <- code_to_function('scripts/02-landcover-scale.R')
 figure_lc <- code_to_function('scripts/figures/figure-lc.R')
 
 
+# Plan
 plan <- drake_plan(
-  data_prep = data_prep)
-# ,
-#   landcover_scale = landcover_scale(),
-#   figure_lc = figure_lc(landcover_scale)
-# )
-
-# plan
-
-make(plan, verbose = 1)
-
+  data_prep = data_prep,
+  landcover_scale = landcover_scale(data_prep),
+  figure_lc = figure_lc(landcover_scale)
+)
