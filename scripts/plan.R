8ded0a68cd5228fec65d199485b7e16bbb39201c
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-16 16:43:06 -0230

    nice sections

--- scripts/plan.R
+++ scripts/plan.R
@@ -1,17 +1,20 @@
-# Load drake
+# Load drake --------------------------------------------------------------
 library(drake)
 
-# Source universal variables and figure theme
+
+# Source universal variables and figure theme -----------------------------
 source('scripts/figures/theme.R')
 source('scripts/00-variables.R')
 
-# Setup scripts as functions
+
+# Setup scripts as functions ----------------------------------------------
 data_prep <- code_to_function('scripts/01-data-prep.R')
 landcover_scale <- code_to_function('scripts/02-landcover-scale.R')
 figure_lc <- code_to_function('scripts/figures/figure-lc.R')
 
 
-# Plan
+
+# Plan --------------------------------------------------------------------
 plan <- drake_plan(
   data = data_prep,
   lc = landcover_scale(data),
