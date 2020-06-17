6e41975a6ce174684e48d441ec4d5dc3dc995ec6
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-17 18:56:29 -0230

    mv scales to pkgs

--- scripts/figures/figure-temp.R
+++ scripts/figures/figure-temp.R
@@ -6,6 +6,7 @@
 # Packages ----------------------------------------------------------------
 pkgs <- c('data.table',
           'ggplot2',
+          'scales',
           'patchwork',
           'ggnetwork')
 p <- lapply(pkgs, library, character.only = TRUE)
@@ -38,7 +39,7 @@ gprop <- ggplot(DT) +
   base +
   labs(x = NULL, y = 'Edge Overlap', subtitle = 'A)') + 
   ylim(0, 1) +
-  scale_x_date(expand = c(0, 0), breaks = scales::pretty_breaks(n = 10))
+  scale_x_date(expand = c(0, 0), breaks = pretty_breaks(n = 10))
 
 
 
@@ -54,7 +55,7 @@ gstr <- ggplot(DT) +
   guides(color = FALSE) +
   base +
   labs(x = xlab, y = 'Graph Strength', subtitle = 'C)') +
-  scale_x_date(expand = c(0, 0), breaks = scales::pretty_breaks(n = 10))
+  scale_x_date(expand = c(0, 0), breaks = pretty_breaks(n = 10))
 
 
 # Layer similarity
@@ -66,7 +67,7 @@ gstr <- ggplot(DT) +
 #     guides(color = FALSE) +
 #     base +
 #     labs(x = xlab, y = 'Layer Similarity') +
-#   scale_x_date(expand = c(0, 0), breaks = scales::pretty_breaks(n = 10))
+#   scale_x_date(expand = c(0, 0), breaks = pretty_breaks(n = 10))
 
 # Network
 gnn <- ggplot(
