7ff2748a58fd8c102934eaee194a9f5a2aeb8fee
Alec L. Robitaille <robit.alec@gmail.com>
2020-06-18 16:50:28 -0230

    revert back to sourcing variables

--- scripts/plan.R
+++ scripts/plan.R
@@ -6,14 +6,12 @@ library(drake)
 # Set renv option for auto snapshots 
 options(renv.config.auto.snapshot = TRUE)
 
+# Source universal variables and figure theme -----------------------------
+source('scripts/figures/theme.R')
+source('scripts/00-variables.R')
+
 
 # Setup scripts as functions ----------------------------------------------
-# Variables
-get_vars <- code_to_function('scripts/00-variables.R')
-
-# Figure themes
-get_theme <- code_to_function('scripts/figures/theme.R')
-
 # Data prep
 data_prep <- code_to_function('scripts/01-data-prep.R')
 
@@ -44,33 +42,27 @@ plan <- drake_plan(
   # Package
   scalepkg = expose_imports('ScaleInMultilayerNetworks'),
   
-  # Vars
-  vars = get_vars(),
-  
-  # Theme
-  theme = get_theme(),
-  
   # Prep data
-  data = data_prep(scalepkg, vars),
+  data = data_prep(scalepkg),
   
   # Figure 1 
   # figure1 = fig_1(), 
   
   # Land cover
-  lc = scale_lc(data, vars),
-  lc_figure = fig_lc(lc, vars, theme),
+  lc = scale_lc(data),
+  lc_figure = fig_lc(lc),
   
   # Temporal
-  temp = scale_temp(data, vars),
-  temp_figure = fig_temp(temp, vars, theme),
+  temp = scale_temp(data),
+  temp_figure = fig_temp(temp),
   
   # Social threshold
-  soc = scale_soc(data, vars),
-  soc_figure = fig_soc(soc, vars, theme),
+  soc = scale_soc(data),
+  soc_figure = fig_soc(soc),
   
   # Number of observations
-  nobs = scale_nobs(data, vars),
-  nobs_figure = fig_nobs(nobs, vars, theme),
+  nobs = scale_nobs(data),
+  nobs_figure = fig_nobs(nobs),
   
   # TODO: fogo car ml
   
