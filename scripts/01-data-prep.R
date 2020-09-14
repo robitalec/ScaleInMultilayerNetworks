9112e151b2d7f66d17c7d4dc7631b043548c5756
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-14 16:26:52 -0230

    rm old prep window len etc

--- scripts/01-data-prep.R
+++ scripts/01-data-prep.R
@@ -64,7 +64,7 @@ reclass <- reclassify(mlc, rcl, include.lowest = TRUE)
 
 
 # Sample land cover -------------------------------------------------------
-### Landcover sample ----
+## Landcover sample
 DT[, (lccol) := extract(reclass, matrix(c(EASTING, NORTHING), ncol = 2))]
 
 
@@ -75,59 +75,10 @@ sub <- DT[(Year == 2017 & JDate > 110) |
             Year == 2018 |
             (Year == 2019 & JDate < 87)]
 
-# ggplot(DT[, uniqueN(ANIMAL_ID), .(d = week(datetime), y = year(datetime))]) + geom_point(aes(d, V1, color = factor(y)))
-# DT[, unique(ANIMAL_ID), .(year(datetime), week(datetime))][, .N, V1]
-# 2017 1 -> 110 =  10
-# 2017 110 -> 2018 265 =  19
-# 2018 265 -> 2019 87 = 15
-# 2019 88 and on = 9
-
-## Which individuals have data in both seasons?
-# TODO: drop if drop time sensitivity
-# Window length
-# l <- lapply(winlengths, function(l) {
-#   col <- paste0('season', l)
-#   
-#   sub[between(JDate, winterlow, winterlow + l), (col) := 'winter']
-#   sub[between(JDate, summerlow, summerlow + l), (col) := 'summer']
-#   
-#   # Fake output
-#   l
-# })
-
-# winlendrop <- lapply(paste0('season', winlengths), function(col) {
-#   sub[, sum(!is.na(get(col))), c(idcol, col)][!is.na(get(col)), .N, idcol][N < 2, get(idcol)]
-# })
-
-# sub[, (grep('season[1-9]', colnames(sub), value = TRUE)) := NULL]
-
-# Window position
-# l <- lapply(winpositions, function(pos) {
-#   col <- paste0('season', pos)
-#   
-#   sub[between(JDate, winterlow + pos, winterlow + pos + winlength), (col) := 'winter']
-#   sub[between(JDate, summerlow + pos, summerlow + pos + winlength), (col) := 'summer']
-#   
-#   # Fake output
-#   pos
-# })
-
-# winposdrop <- lapply(paste0('season', winpositions), function(col) {
-#   sub[, sum(!is.na(get(col))), c(idcol, col)][!is.na(get(col)), .N, idcol][N < 2, get(idcol)]
-# })
-
-# sub[, (grep('season[1-9]', colnames(sub), value = TRUE)) := NULL]
-
-
-# Combine lists
-# dropids <- unique(c(unlist(winlendrop), unlist(winposdrop)))
-
-# sub <- sub[!get(idcol) %in% dropids]
 subseasons <- sub[!is.na(season)]
 
 
 # Output ------------------------------------------------------------------
 saveRDS(sub, 'data/derived-data/01-sub-fogo-caribou.Rds')
 saveRDS(subseasons, 'data/derived-data/01-sub-seasons-fogo-caribou.Rds')
-# saveRDS(all, 'data/derived-data/01-all-fogo-caribou.Rds')
 saveRDS(reclass, 'data/derived-data/01-reclass-lc.Rds')
